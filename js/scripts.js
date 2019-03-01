830eb55e37ceed9912bd60a2eedfaf8c6dd6f439
Ryan McLean <mcleanrs@comcast.net>
2019-03-01 13:20:35 -0800

    add slideshow

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,21 @@ $(document).ready(function() {
     $(".searchInput").slideToggle("slow");
   });
 });
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
